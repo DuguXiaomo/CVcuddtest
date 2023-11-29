08fde4ff8e5ba19c4b9488ad72c60eed08fe9cc5
DuguXiaomo <DuguXiaomo@users.noreply.github.com>
2023-11-29 15:29:06 +0800

    fifth commit

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 # CUDD 库
-CUDD_INCLUDE_PATH=/path/to/cudd/include
-CUDD_LIB_PATH=/path/to/cudd/lib
+CUDD_INCLUDE_PATH=/home/public/include
+CUDD_LIB_PATH=/home/public/lib
+
 
 # cJSON 库
-CJSON_INCLUDE_PATH=/path/to/cJSON
-CJSON_LIB_PATH=/path/to/cJSON
+CJSON_INCLUDE_PATH=/home/eda230906/cJSON.h
+CJSON_LIB_PATH=/home/eda230906/cJSON.c
 
-gcc -o bdd_processing src/main.c src/cudd_functions.c src/json_functions.c \
+gcc -o bdd_processing test.c cudd.c cJSON.c \
 -I$CUDD_INCLUDE_PATH -L$CUDD_LIB_PATH -lcudd \
 -I$CJSON_INCLUDE_PATH -L$CJSON_LIB_PATH -lcjson -lm -std=c11
